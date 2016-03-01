3e18c665329dbffed47d9e15cee6507b3b76eb79
gabu <tsukada.shouya@gmail.com>
2016-03-01 18:27:43 +0900

    Added shared framework target

---
+++ Example/AnnotationClusteringFramework/AnnotationClusteringFramework.h
@@ -0,0 +1,19 @@
+//
+//  AnnotationClusteringFramework.h
+//  AnnotationClusteringFramework
+//
+//  Created by Shoya Tsukada on 2016/03/01.
+//  Copyright © 2016年 Infinum Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnnotationClusteringFramework.
+FOUNDATION_EXPORT double AnnotationClusteringFrameworkVersionNumber;
+
+//! Project version string for AnnotationClusteringFramework.
+FOUNDATION_EXPORT const unsigned char AnnotationClusteringFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnnotationClusteringFramework/PublicHeader.h>
+
+
